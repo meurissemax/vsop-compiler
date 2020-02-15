8240235539002a7d3e127dbf0290c76be4fa1759
Valentin <valentin.vermeylen@student.ulg.ac.be>
2020-02-15 12:02:02 +0100

    Lexer : end of regex and tables of symbols

--- 1/python/lexer.py
+++ 1/python/lexer.py
@@ -20,11 +20,43 @@ from rply.errors import LexingError
 # Classes #
 ###########
 
+
 class Lexer():
     def __init__(self):
         self.lexer = LexerGenerator()
+        self.keywords = ('and', 'bool', 'class', 'do', 'else', 'extends', 'false', 'if', 
+            'in', 'int32', 'isnull', 'let', 'new', 'not', 'string', 'then', 'true', 'unit', 'while')
+        self.op = {
+                    r'{' : 'lbrace',
+                    r'}' : 'rbrace',
+                    r'(' : 'lpar',
+                    r')' : 'rpar',
+                    r':' : 'colon',
+                    r';' : 'semicolon',
+                    r',' : 'comma',
+                    r'\+' : 'plus',
+                    r'-' : 'minus',
+                    r'\*' : 'times',
+                    r'/' : 'div',
+                    r'\^' : 'pow',
+                    r'\.' : 'dot',
+                    r'=' : 'equal',
+                    r'<' : 'lower',
+                    r'<=' : 'lower-equal',
+                    r'<-' : 'assign'
+                    }
+
+    def isKeyword(element):
+        return (element in self.keywords)
+
+    def whichOp(element):
+        if not element in self.op.keys():
+            # Raise exception ? Should not happen
+            pass
+        return self.op[element]
 
     def _add_tokens(self):
+        
         self.lexer.add('lowercase-letter', r'[a-z]')
         self.lexer.add('uppercase-letter', r'[A-Z]')
         self.lexer.add('letter', r'[a-z]|[A-Z]')
@@ -38,7 +70,6 @@ class Lexer():
         self.lexer.add('comment-right', r'\*\)') # Call a second lexer when inside a comment.
 
         self.lexer.add('integer-literal', r'0x([0-9]|[a-f]|[A-F])+|[0-9]+')
-        #self.lexer.add('keyword', ) Le faire ou garder une table et checker que c'est un keyword quand on appelle le lexer dans la boucle?
         
         self.lexer.add('type-identifier', r'[A-Z](([a-z]|[A-Z])|[0-9]|_)*')
 
@@ -49,23 +80,8 @@ class Lexer():
         escaped_char = r'\\' + r'[' + escape_sequence + r']'
         self.lexer.add('string-literal', r'\"['+ regular_char + r'|' + escaped_char + r']\"')
         
-        self.lexer.add('lbrace', r'{')
-        self.lexer.add('rbrace', r'}')
-        self.lexer.add('lpar', r'(')
-        self.lexer.add('rpar', r')')
-        self.lexer.add('colon', r':')
-        self.lexer.add('semicolon', r';')
-        self.lexer.add('comma', r',')
-        self.lexer.add('plus', r'\+')
-        self.lexer.add('minus', r'-')
-        self.lexer.add('times', r'\*') ## OU EN FAIRE UN SEUL TOKEN ET GARDER UN TABLEAU. JE NE SAIS
-        self.lexer.add('div', r'\\')   ## PAS CE QUI EST LE MIEUX
-        self.lexer.add('pow', r'\^')
-        self.lexer.add('dot', r'\.')
-        self.lexer.add('equal', r'=')
-        self.lexer.add('lower', r'<')
-        self.lexer.add('lower-equal', r'<=')
-        self.lexer.add('assign', r'<-')
+        self.lexer.add('operator', r'[{}():;,\+-\*\\\^\.=<]|<=|<-')
+
 
     def get_lexer(self):
         self._add_tokens()
