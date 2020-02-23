52a197d47d63dba2004fb870cdd1e04bd4a19082
Maxime Meurisse <max.meurisse@outlook.com>
2020-02-23 18:50:51 +0100

    Lexer : fully functional lexer (40/40 on the plateform)

--- 1/python/lexer.py
+++ 1/python/lexer.py
@@ -51,8 +51,9 @@ class Lexer():
             'string_literal',
             'operator',
 
-            # Special token for keywords and type identifiers
-            'identifier'
+            # Special tokens
+            'identifier',
+            'non_terminated_string_literal'
         )
 
         # We define keywords
@@ -104,6 +105,10 @@ class Lexer():
         self.tokens += list(self.__keywords)
         self.tokens += list(self.__operators.values())
 
+        # We set a possible state of the lexer (to handle
+        # comment)
+        self.states = (('comment', 'exclusive'),)
+
         # Flag to handle single-line comments
         self.__s_comments = False
 
@@ -138,7 +143,7 @@ class Lexer():
 
         return self.__is_s_comment() or self.__is_m_comment()
 
-    def t_inline_comment(self, t):
+    def t_ANY_inline_comment(self, t):
         r'\/\/'
 
         # We only consider the token as an
@@ -146,10 +151,11 @@ class Lexer():
         # already in a comment
         if not self.__is_comment():
             self.__s_comments = True
+            t.lexer.begin('comment')
 
         pass
 
-    def t_left_comment(self, t):
+    def t_ANY_left_comment(self, t):
         r'\(\*'
 
         # If we are not already in a single-line
@@ -157,10 +163,11 @@ class Lexer():
         # multiple-line comment
         if not self.__is_s_comment():
             self.__m_comments.append((t.lineno, self.__find_column(t)))
+            t.lexer.begin('comment')
 
         pass
 
-    def t_right_comment(self, t):
+    def t_ANY_right_comment(self, t):
         r'\*\)'
 
         # We check if we are not already in a
@@ -184,6 +191,12 @@ class Lexer():
             else:
                 self.__m_comments.pop()
 
+                # If we are not anymore in a comment (stack
+                # __m_comments empty), we come back to the
+                # initial state of the lexer
+                if not self.__is_comment():
+                    t.lexer.begin('INITIAL')
+
         pass
 
     ###################
@@ -196,15 +209,10 @@ class Lexer():
     # second, etc.
 
     # Characters to ignore during lexing
-    t_ignore = '[ \r\f\t]'
+    t_ANY_ignore = ' \r\f\t'
 
     def t_identifier(self, t):
         r'[a-z](([a-z]|[A-Z])|[0-9]|_)*'
-
-        # If we are in a comment, we do not return
-        # the token, we just keep executing
-        if self.__is_comment():
-            return
 
         # Identifer can be keyword or object identifier,
         # so we have to check
@@ -218,20 +226,10 @@ class Lexer():
     def t_type_identifier(self, t):
         r'[A-Z](([a-z]|[A-Z])|[0-9]|_)*'
 
-        # If we are in a comment, we do not return
-        # the token, we just keep executing
-        if self.__is_comment():
-            return
-
         return t
 
     def t_integer_literal(self, t):
         r'([0-9]|0x)([0-9]|[a-z]|[A-Z])*'
-
-        # If we are in a comment, we do not return
-        # the token, we just keep executing
-        if self.__is_comment():
-            return
 
         # We check if the value is an decimal or
         # hexadecimal value in order to make a
@@ -260,27 +258,23 @@ class Lexer():
                 t.lexer.skip(1)
 
     def t_string_literal(self, t):
-        r'\"(?!\\\")(.|\n)*\"'
+        r'\"(?:[^\"\\]|\\.|\\\n)*\"'
 
-        # If we are in a comment, we do not return
-        # the token, we just keep executing
-        if self.__is_comment():
-            return
+        # We returns the processed string
+        return self.__string_processing(t)
 
-        t.lexer.lineno += t.value.count('\n')
+    def t_non_terminated_string_literal(self, t):
+        r'\"(?:[^\"\\]|\\.|\\\n)*'
 
-        # We escape special characters
-        t.value = re.sub(r'(\s\s|\s\\\s|\n)*', '', t.value)
+        # We process the string (to check if there is an error)
+        # but we do not return her because we know that if there
+        # is no error during processing, there is still an error
+        # due to the fact that we are in the case of a non-
+        # terminated string
+        t = self.__string_processing(t)
 
-        t.value = t.value.replace(r'\b', r'\x08')
-        t.value = t.value.replace(r'\t', r'\x09')
-        t.value = t.value.replace(r'\n', r'\x0a')
-        t.value = t.value.replace(r'\r', r'\x0d')
-
-        t.value = t.value.replace(r'\"', r'\x22')
-        t.value = t.value.replace(r'\\', r'\x5c')
-
-        return t
+        # We print the error of the non-terminated string
+        self.__print_error(t.lineno, self.__find_column(t), 'string non terminated before end of file.')
 
     def t_operator(self, t):
         r'{|}|\(|\)|:|;|,|\+|-|\*|/|\^|\.|<=|<-|<|='
@@ -288,11 +282,6 @@ class Lexer():
         # Remark : we put '<=' and '<-' before
         # '<' and '=' in the regex to be sure
         # that they match before
-
-        # If we are in a comment, we do not return
-        # the token, we just keep executing
-        if self.__is_comment():
-            return
 
         # Since the regex "hardcode" all the operators,
         # we don't really have to check if the value
@@ -305,7 +294,7 @@ class Lexer():
     # Newlines management #
     #######################
 
-    def t_newline(self, t):
+    def t_ANY_newline(self, t):
         r'[\n]+'
 
         # The rule to update the number of the line
@@ -313,9 +302,12 @@ class Lexer():
         t.lexer.lineno += t.value.count('\n')
 
         # If we are in a single-line comment, a new
-        # line ends the comment
+        # line ends the comment and push back the
+        # lexer to its initial state
+
         if self.__is_s_comment():
             self.__s_comments = False
+            t.lexer.begin('INITIAL')
 
     ##################
     # Error handling #
@@ -329,7 +321,7 @@ class Lexer():
 
         utils.print_error('{}:{}:{}: lexical error: {}'.format(self.__filename, lineno, column, message))
 
-    def t_error(self, t):
+    def t_ANY_error(self, t):
         # If we are in a comment, we don't print errors
         # (because we will have a lot of error since
         # token are not recognised in comment)
@@ -353,6 +345,46 @@ class Lexer():
         line_start = self.__data.rfind('\n', 0, t.lexpos) + 1
 
         return (t.lexpos - line_start) + 1
+
+    def __string_processing(self, t):
+        """
+        Process a string literal according to
+        the brief (replace valid newline character,
+        escape character and check unknow character).
+        """
+
+        t.lexer.lineno += t.value.count('\n')
+
+        # We remove (valid) newline character for string (\)
+        t.value = re.sub(r'(\\\n( )*|\\\n)', '', t.value)
+
+        # We check if the treated string contains line feed
+        if '\n' in t.value:
+            self.__print_error(t.lineno, self.__find_column(t) + t.value.find('\n'), 'string contains line feed.')
+
+        # We check if the treated string contains null character
+        if len(t.value.split('\x00')) is not 1:
+            splits = t.value.split('\x00')
+            self.__print_error(t.lineno, self.__find_column(t) + len(splits[0]), 'string contains null character.')
+
+        # We escape special characters
+        t.value = t.value.replace(r'\b', r'\x08')
+        t.value = t.value.replace(r'\t', r'\x09')
+        t.value = t.value.replace(r'\n', r'\x0a')
+        t.value = t.value.replace(r'\r', r'\x0d')
+
+        t.value = t.value.replace(r'\"', r'\x22')
+        t.value = t.value.replace(r'\\', r'\x5c')
+
+        # We check if the treated string contains an unknow escaped character
+        if t.value.find('\\') is not -1:
+            pos = t.value.find('\\')
+            escaped = t.value[pos:pos + 4]
+
+            if escaped not in (r'\x08', r'\x09', r'\x0a', r'\x0d', r'\x22', r'\x5c'):
+                self.__print_error(t.lineno, self.__find_column(t) + pos, 'unknow escaped character.')
+
+        return t
 
     ###########################
     # Build and use the lexer #
